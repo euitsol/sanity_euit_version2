d81415757244f2f173383d874c8e94cc59f7fd2f
Sandeep Chowdhury <euitsols.suprt@gmail.com>
2022-05-16 15:13:32 +0600

    initialised project

---
+++ sanityeuit/schemas/course_product.js
@@ -0,0 +1,58 @@
+export default {
+    name: 'course_product',
+    title: 'Course_Product',
+    type: 'document',
+    fields: [
+      {
+        name: 'course_name',
+        title: 'Course Name',
+        type: 'string',
+      },
+      {
+        name: 'category',
+        title: 'Category',
+        type: 'string',
+      },
+      {
+        name: 'course_price',
+        title: 'Course Price',
+        type: 'number',
+      },
+      {
+        name: 'course_duration',
+        title: 'Course Duration',
+        type: 'number',
+      },
+  
+      {
+        name: 'image',
+        title: 'Image',
+        type: 'image',
+        options: {
+          hotspot: true,
+        },
+      },
+      {
+        name: 'description',
+        title: 'Description',
+        type: 'string',
+      },
+  
+      {
+        name: 'slug',
+        title: 'Slug',
+        type: 'slug',
+        options: {
+          source: 'course_name',
+          maxLength: 96,
+        },
+      },
+  
+      // {
+      //   name: 'module',
+      //   title: 'Module',
+      //   type: 'string',
+      // },
+    ],
+  };
+  
