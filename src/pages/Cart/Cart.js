11e14cbdba22405c5f9babae033c7d319c852fa9
euitsol <euitsols.suprt@gmail.com>
2022-06-14 21:03:41 +0600

    added cart functionality

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import useCourses from '../../Hooks/useCourses';
-import { getStoredCart } from '../../utilities/localstorage'
+import { getStoredCart, removeFromDb } from '../../utilities/localstorage'
 import Loading from '../Loading/Loading';
 import './Cart.css'
-import CartProduct from './CartProduct';
 
 function Cart() {
   const [courses, loading, error, course_prod] = useCourses();
@@ -26,6 +25,13 @@ function Cart() {
       return <Loading></Loading>
     }
 
+    // delete from cart 
+    const deleteHandler = (slug)=>{
+      removeFromDb(slug)
+      const remainingData = cart.filter(cartitem=> cartitem.slug.current != slug);
+      setCart(remainingData);
+    }
+
   return (
     <div>
       <div className='cart-title'>
@@ -33,7 +39,13 @@ function Cart() {
       </div>
       <div className='cart-items'>
         {
-          cart.map((course, i)=><CartProduct key={i} course={course}></CartProduct>)
+          cart.map((course, i)=><div className='cart-card'>
+          <img className='course-img' src={course.image.asset.url}></img>
+          <p className='course-name-cart'>{course.course_name}</p>
+          <p>{course.course_price}</p>
+          <input type="number" id="quantity" name="quantity" min="0" max="100" step="1" value={course.quantity} />
+          <button onClick={()=>deleteHandler(course.slug.current)} className='cart-item-delete-btn'>Delete</button>
+        </div>)
         }
       </div>
     </div>
